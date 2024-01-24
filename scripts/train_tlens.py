ae58bf624d7d445be8d99fdf08921674c53d9287
Dan Braun <dan@apolloresearch.ai>
2024-01-24 18:12:29 +0000

    Config for tflens HookedTransformer working with SAEs

--- scripts/train_tlens.py
+++ scripts/train_tlens.py
@@ -1,14 +1,43 @@
+from typing import Any, Literal, Optional
+
 import fire
 import torch
 from jaxtyping import Float
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, field_validator, model_validator
 from torch import Tensor, nn
-from transformer_lens import HookedTransformer, evals
+from transformer_lens import HookedTransformer, HookedTransformerConfig, evals
 
 from sparsify.hook_fns import sae_acts_pre_forward_hook_fn
 from sparsify.hook_manager import Hook, HookedModel
 from sparsify.models.sparsifiers import SAE
 from sparsify.utils import load_config
+
+StrDtype = Literal["float32", "float64", "bfloat16"]
+TORCH_DTYPES: dict[StrDtype, torch.dtype] = {
+    "float32": torch.float32,
+    "float64": torch.float64,
+    "bfloat16": torch.bfloat16,
+}
+
+
+class HookedTransformerPreConfig(BaseModel):
+    """Pydantic model whose arguments will be passed to a HookedTransformerConfig."""
+
+    model_config = ConfigDict(extra="forbid", arbitrary_types_allowed=True, frozen=True)
+    d_model: int
+    n_layers: int
+    n_ctx: int
+    d_head: int
+    d_vocab: int
+    act_fn: str
+    dtype: Optional[torch.dtype]
+
+    @field_validator("dtype", mode="before")
+    @classmethod
+    def dtype_to_torch_dtype(cls, v: str) -> torch.dtype:
+        if v not in TORCH_DTYPES:
+            raise ValueError(f"Invalid dtype {v}.")
+        return TORCH_DTYPES[v]
 
 
 class TrainConfig(BaseModel):
@@ -17,19 +46,28 @@ class TrainConfig(BaseModel):
 
 
 class Config(BaseModel):
-    tlens_model_name: str
+    model_config = ConfigDict(extra="forbid", frozen=True)
+    tlens_model_name: Optional[str] = None
+    tlens_config: Optional[HookedTransformerPreConfig] = None
     input_size: int
     n_dict_components: int
     sae_orig: bool
     sae_sparsity: bool
     train: TrainConfig
 
+    @model_validator(mode="before")
+    @classmethod
+    def check_only_one_model_definition(cls, values: dict[str, Any]) -> dict[str, Any]:
+        assert (values["tlens_model_name"] is None) + (
+            values["tlens_config"] is None
+        ) == 1, "Must specify exactly one of tlens_model_name or tlens_config."
+        return values
+
 
 class SAETransformer(nn.Module):
-    def __init__(self, config: Config):
+    def __init__(self, tflens_model: HookedTransformer, config: Config):
         super().__init__()
-        self.tlens_model = HookedTransformer.from_pretrained(config.tlens_model_name)
-
+        self.tlens_model = tflens_model
         self.saes = nn.ModuleDict()
         for i in range(self.tlens_model.cfg.n_layers):
             self.saes[str(i)] = SAE(
@@ -144,7 +182,12 @@ def main(config_path_str: str) -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     config = load_config(config_path_str, config_model=Config)
 
-    model = SAETransformer(config).to(device=device)
+    if config.tlens_model_name is not None:
+        tlens_model = HookedTransformer.from_pretrained(config.tlens_model_name)
+    else:
+        hooked_transformer_config = HookedTransformerConfig(**config.tlens_config.model_dump())
+        tlens_model = HookedTransformer(hooked_transformer_config)
+    model = SAETransformer(tlens_model, config).to(device=device)
     hooked_model = HookedModel(model)
     train(config, hooked_model, device=device)
 
