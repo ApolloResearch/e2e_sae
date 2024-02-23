8bc3b04dd9c114854402f5394f8b9b88374e186f
Dan Braun <dan@apolloresearch.ai>
2024-02-23 12:23:53 +0000

    Log the trainable parameters

--- sparsify/scripts/train_tlens_saes/run_train_tlens_saes.py
+++ sparsify/scripts/train_tlens_saes/run_train_tlens_saes.py
@@ -413,6 +413,7 @@ def main(config_path_or_obj: Path | str | Config) -> None:
             ]
             assert len(trainable_param_names) > 0, "No trainable parameters found."
 
+    logger.info(f"Trainable parameters: {trainable_param_names}")
     train(
         config=config,
         model=model,
