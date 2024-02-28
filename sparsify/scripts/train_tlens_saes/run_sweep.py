a674abaebd948b86a5c0f510330012a7c263abe4
Jordan Taylor <nadroj@live.com.au>
2024-02-27 18:21:31 -0800

    First runs of gpt2-small_compare-with-joseph_layerwise_2024_02_27
     https://wandb.ai/sparsify/gpt2-small_compare-with-joseph_layerwise_2024_02_27

--- sparsify/scripts/train_tlens_saes/run_sweep.py
+++ sparsify/scripts/train_tlens_saes/run_sweep.py
@@ -15,21 +15,61 @@ from sparsify.utils import replace_pydantic_model
 
 def main(config_path_str: str) -> None:
     """Run the training script with different sae_position_name values."""
-    sweep_name = "tinystories-1m_sparsity-coeff"
-    values = [1.0, 0.1, 0.01, 0.001, 0.0001, 0.00001]
+    sweep_name = "gpt2-small_compare-with-joseph_layerwise_2024_02_27"
+    update_dicts =  [
+            {   
+                "train":{
+                    "lr": 0.0004,
+                    "loss_configs":{
+                        "sparsity": {
+                            "coeff": 0.00008
+                        }
+                    },
+                },
+                "saes": {
+                    "sae_position_names": "blocks.2.hook_resid_post"
+                },
+                "wandb_project": sweep_name,
+            },
+            #########################################
+            {   
+                "train":{
+                    "lr": 0.004,
+                    "loss_configs":{
+                        "sparsity": {
+                            "coeff": 0.00008
+                        }
+                    },
+                },
+                "saes": {
+                    "sae_position_names": "blocks.2.hook_resid_post"
+                },
+                "wandb_project": sweep_name,
+            },
+            #########################################
+            {   
+                "train":{
+                    "lr": 0.0004,
+                    "loss_configs":{
+                        "sparsity": {
+                            "coeff": 0.0008
+                        }
+                    },
+                },
+                "saes": {
+                    "sae_position_names": "blocks.2.hook_resid_post"
+                },
+                "wandb_project": sweep_name,
+            },
+    ]
 
     with open(config_path_str) as f:
         base_config = Config(**yaml.safe_load(f))
 
-    for value in values:
-        update_dict = {
-            "train": {"loss_configs": {"sparsity": {"coeff": value}}},
-            "wandb_project": sweep_name,
-        }
+    for update_dict in update_dicts:
         new_config = replace_pydantic_model(base_config, update_dict)
         print(new_config)
         run_train(new_config)
-
 
 if __name__ == "__main__":
     Fire(main)
