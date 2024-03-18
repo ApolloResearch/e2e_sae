e7f3025f12b4c034a7d55db35ad4428bc9b49a13
Dan Braun <dan@apolloresearch.ai>
2024-03-18 12:37:11 +0000

    Make gpu indices optional in run_wandb_sweep

--- sparsify/scripts/train_tlens_saes/run_wandb_sweep.py
+++ sparsify/scripts/train_tlens_saes/run_wandb_sweep.py
@@ -1,18 +1,35 @@
 """Run wandb sweep agents in parallel using tmux sessions.
 
 Usage:
-    python run_wandb_sweep.py <wandb_agent_id>
+    The script can be invoked from the command line, providing the wandb agent ID and optionally,
+    a comma-separated list of GPU indices. If no GPU indices are provided, the script will
+    automatically use all available GPUs.
+
+    Example command to run the script specifying GPUs:
+        python run_wandb_sweep.py your_agent_id 0,1,4
+
+    Example command to run the script using all available GPUs:
+        python run_wandb_sweep.py your_agent_id
+
 """
 import subprocess
 
+import torch
 from fire import Fire
 
 
-def main(agent_id: str) -> None:
-    """Run the training script with different GPU indices.
-    NOTE: You must specify the GPU indices to use in the `gpu_idxs` list.
+def main(agent_id: str, gpu_idxs_str: str = "") -> None:
+    """Run the training script with specified GPU indices.
+
+    Args:
+        agent_id: The wandb agent ID.
+        gpu_idxs: A comma-separated string of GPU indices to use. Use all available GPUs if empty.
     """
-    gpu_idxs = [0, 1, 2, 3, 4, 5]
+    # Check if the user has specified GPU indices
+    if gpu_idxs_str == "":
+        gpu_idxs = [int(idx) for idx in gpu_idxs_str.split(",")]
+    else:
+        gpu_idxs = list(range(torch.cuda.device_count()))
 
     for idx in gpu_idxs:
         session_exists = subprocess.run(f"tmux has-session -t {idx}".split(), capture_output=True)
