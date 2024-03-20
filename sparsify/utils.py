bfdcfdd0ca8c32b60d9d0c20dc5b7311b2a3a01f
Dan Braun <dan@apolloresearch.ai>
2024-03-20 11:29:47 +0000

    Set min-lr for cosine and linear schedules

--- sparsify/utils.py
+++ sparsify/utils.py
@@ -2,6 +2,7 @@ import math
 import os
 import random
 from collections.abc import Callable
+from functools import partial
 from pathlib import Path
 from typing import Any, TypeVar
 
@@ -13,6 +14,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from pydantic.v1.utils import deep_update
 from torch import nn
+from torch.optim.lr_scheduler import LambdaLR
 from transformer_lens import HookedTransformer
 from transformer_lens.hook_points import HookPoint
 
@@ -159,11 +161,14 @@ def get_hook_shapes(tlens_model: HookedTransformer, hook_names: list[str]) -> di
 
 
 def get_linear_lr_schedule(
-    warmup_samples: int, cooldown_samples: int, n_samples: int | None, effective_batch_size: int
+    warmup_samples: int,
+    cooldown_samples: int,
+    n_samples: int | None,
+    effective_batch_size: int,
+    min_lr_factor: float = 0.0,
 ) -> Callable[[int], float]:
     """
     Generates a linear learning rate schedule function that incorporates warmup and cooldown phases.
-
     If warmup_samples and cooldown_samples are both 0, the learning rate will be constant at 1.0
     throughout training.
 
@@ -171,6 +176,8 @@ def get_linear_lr_schedule(
         warmup_samples: The number of samples to use for warmup.
         cooldown_samples: The number of samples to use for cooldown.
         effective_batch_size: The effective batch size used during training.
+        min_lr_factor: The minimum learning rate as a fraction of the maximum learning rate. Used
+            in the cooldown phase.
 
     Returns:
         A function that takes a training step as input and returns the corresponding learning rate.
@@ -207,12 +214,80 @@ def get_linear_lr_schedule(
             # Calculate how many steps have been taken in the cooldown phase
             steps_into_cooldown = step - cooldown_start
             # Linearly decrease the learning rate
-            return max(0.0, 1 - (steps_into_cooldown / cooldown_steps))
+            return max(min_lr_factor, 1 - (steps_into_cooldown / cooldown_steps))
         else:
             # Maintain maximum learning rate after warmup and before cooldown
             return 1.0
 
     return lr_schedule
+
+
+def _get_cosine_schedule_with_warmup_lr_lambda(
+    current_step: int,
+    *,
+    num_warmup_steps: int,
+    num_training_steps: int,
+    num_cycles: float,
+    min_lr_factor: float,
+):
+    if current_step < num_warmup_steps:
+        return float(current_step) / float(max(1, num_warmup_steps))
+    progress = float(current_step - num_warmup_steps) / float(
+        max(1, num_training_steps - num_warmup_steps)
+    )
+    return max(
+        min_lr_factor,
+        min_lr_factor
+        + (1 - min_lr_factor)
+        * 0.5
+        * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)),
+    )
+
+
+def get_cosine_schedule_with_warmup(
+    optimizer: torch.optim.Optimizer,
+    num_warmup_steps: int,
+    num_training_steps: int,
+    num_cycles: float = 0.5,
+    min_lr_factor: float = 0.0,
+    last_epoch: int = -1,
+):
+    """
+    Create a schedule with a learning rate that decreases following the values of the cosine
+    function between the initial lr set in the optimizer to 0, after a warmup period during which it
+    increases linearly between 0 and the initial lr set in the optimizer.
+
+    The min_lr_factor is used to set a minimum learning rate that is a fraction of the initial
+    learning rate.
+
+    Adapted from `transformers.get_cosine_schedule_with_warmup` to support a minimum learning rate.
+
+    Args:
+        optimizer ([`~torch.optim.Optimizer`]):
+            The optimizer for which to schedule the learning rate.
+        num_warmup_steps (`int`):
+            The number of steps for the warmup phase.
+        num_training_steps (`int`):
+            The total number of training steps.
+        num_cycles (`float`, *optional*, defaults to 0.5):
+            The number of waves in the cosine schedule (the defaults is to just decrease from the
+            max value to 0 following a half-cosine).
+        min_lr_factor (`float`, *optional*, defaults to 0.0):
+        last_epoch (`int`, *optional*, defaults to -1):
+            The index of the last epoch when resuming training.
+
+    Return:
+        `torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
+    """
+
+    lr_lambda = partial(
+        _get_cosine_schedule_with_warmup_lr_lambda,
+        num_warmup_steps=num_warmup_steps,
+        num_training_steps=num_training_steps,
+        num_cycles=num_cycles,
+        min_lr_factor=min_lr_factor,
+    )
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
 
 
 def init_wandb(config: T, project: str, sweep_config_path: Path | str | None) -> T:
