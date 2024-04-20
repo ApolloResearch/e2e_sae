5c17d0ba18fa6cf6479f1f9b0bd8e10a2f3743eb
Dan Braun <dan@apolloresearch.ai>
2024-04-20 13:14:23 +0000

    Mark test_generate as cpuslow

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ Taken from https://docs.pytest.org/en/latest/example/simple.html.
 from collections.abc import Iterable
 
 import pytest
+import torch
 from pytest import Config, Item, Parser
 
 
@@ -14,6 +15,7 @@ def pytest_addoption(parser: Parser) -> None:
 
 def pytest_configure(config: Config) -> None:
     config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line("markers", "cpuslow: mark test as slow to run if no gpu available")
 
 
 def pytest_collection_modifyitems(config: Config, items: Iterable[Item]) -> None:
@@ -21,6 +23,9 @@ def pytest_collection_modifyitems(config: Config, items: Iterable[Item]) -> None
         # --runslow given in cli: do not skip slow tests
         return
     skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    skip_cpuslow = pytest.mark.skip(reason="need --runslow option to run since no gpu available")
     for item in items:
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
+        if "cpuslow" in item.keywords and not torch.cuda.is_available():
+            item.add_marker(skip_cpuslow)
