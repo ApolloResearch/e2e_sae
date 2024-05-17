edcf2daa9753e7c3d4d3998b8c050c2629605eed
Nix Goldowsky-Dill <nix@apolloresearch.ai>
2024-05-17 09:14:00 +0000

    cleanup resample_direction

--- e2e_sae/scripts/analysis/resample_direction.py
+++ e2e_sae/scripts/analysis/resample_direction.py
@@ -1,3 +1,4 @@
+"""Tests how robust network outputs are to resampling-ablating PCA directions"""
 from collections.abc import Callable
 from functools import partial
 from pathlib import Path
@@ -7,7 +8,7 @@ import torch
 import tqdm
 import transformer_lens as tl
 import wandb
-from jaxtyping import Float
+from jaxtyping import Float, Int
 from transformer_lens import HookedTransformer
 
 from e2e_sae.data import DatasetConfig, create_data_loader
@@ -17,41 +18,7 @@ from e2e_sae.scripts.analysis.plot_settings import SIMILAR_CE_RUNS
 ActTensor = Float[torch.Tensor, "batch seq hidden"]
 LogitTensor = Float[torch.Tensor, "batch seq vocab"]
 DirTensor = Float[torch.Tensor, "hidden"]
-
-# %%
-
-api = wandb.Api()
-local_run_id = SIMILAR_CE_RUNS[10]["local"]
-run = api.run(f"sparsify/gpt2/{local_run_id}")
-acts = get_acts(run)
-pca_dirs = pca(acts.orig.flatten(0, 1), n_dims=None).T
-
-
-# %%
-
-dataset_config = DatasetConfig(
-    dataset_name="apollo-research/Skylion007-openwebtext-tokenizer-gpt2",
-    is_tokenized=True,
-    tokenizer_name="gpt2",
-    streaming=True,
-    split="train",
-    n_ctx=1024,
-    seed=100,
-    column_name="input_ids",
-)
-
-# %%
-gpt2 = HookedTransformer.from_pretrained("gpt2")
-hook_point_name = "blocks.10.hook_resid_pre"
-
-# %%
-
-eot_id = 50256
-
-
-data_loader, _ = create_data_loader(dataset_config, batch_size=30)
-device = "cuda" if torch.cuda.is_available() else "cpu"
-batch = next(iter(data_loader))["input_ids"].to(device)
+TokenTensor = Int[torch.Tensor, "batch seq"]
 
 
 def shuffle_tensor(x: torch.Tensor) -> torch.Tensor:
@@ -71,8 +38,6 @@ def resample_direction_hook(
 ) -> ActTensor:
     seqpos_arr = torch.arange(x.shape[1]).repeat([len(x), 1]).to(device)
     mask = seqpos_arr > 0
-    # mask = (seqpos_arr > 0) & (batch != eot_id)
-
     x_within_dir = (x @ dir).unsqueeze(-1) * dir
     resid = x - x_within_dir
     shuffled_x_within_dir = apply_fn_to_mask(x_within_dir, mask, shuffle_tensor)
@@ -80,30 +45,71 @@ def resample_direction_hook(
 
 
 @torch.no_grad()
-def get_kl_diff_permuting_dir(dir: DirTensor):
-    orig_logits = gpt2(batch)
+def get_kl_diff_permuting_dir(
+    model: HookedTransformer,
+    hook_point_name: str,
+    input_ids: TokenTensor,
+    dir: DirTensor,
+    device: str,
+):
+    input_ids = input_ids.to(device)
+    orig_logits = model(input_ids)
     partial_hook = partial(resample_direction_hook, dir=dir.to(device))
-    hooked_logits = gpt2.run_with_hooks(batch, fwd_hooks=[(hook_point_name, partial_hook)])
+    hooked_logits = model.run_with_hooks(input_ids, fwd_hooks=[(hook_point_name, partial_hook)])
 
     return kl_div(orig_logits, hooked_logits).mean().item()
 
 
-xs = range(50)
-kls = [get_kl_diff_permuting_dir(dir=pca_dirs[x]) for x in tqdm.tqdm(xs)]
+def get_batch():
+    dataset_config = DatasetConfig(
+        dataset_name="apollo-research/Skylion007-openwebtext-tokenizer-gpt2",
+        is_tokenized=True,
+        tokenizer_name="gpt2",
+        streaming=True,
+        split="train",
+        n_ctx=1024,
+        seed=100,
+        column_name="input_ids",
+    )
 
-# %%
-xlim = 25
-plt.plot(xs[:xlim], kls[:xlim], "o-k")
-plt.ylim(0, None)
-plt.xlabel("PCA direction")
-plt.ylabel("KL divergence")
-# plt.title("How much does permuting activations mess up the model?")
-plt.gcf().set_size_inches(4, 3)
+    data_loader, _ = create_data_loader(dataset_config, batch_size=30)
+    return next(iter(data_loader))["input_ids"]
 
-scripts_dir = Path(__file__).parent
-out_dir = scripts_dir / "out/pca_dir_0"
 
-plt.savefig(out_dir / "resample_sensitivity.png", dpi=300, bbox_inches="tight")
-plt.savefig(out_dir / "resample_sensitivity.svg", bbox_inches="tight")
+def get_pca_dirs():
+    api = wandb.Api()
+    local_run_id = SIMILAR_CE_RUNS[10]["local"]
+    run = api.run(f"sparsify/gpt2/{local_run_id}")
+    acts = get_acts(run)
+    return pca(acts.orig.flatten(0, 1), n_dims=None).T
 
-# %%
+
+if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    gpt2 = HookedTransformer.from_pretrained("gpt2")
+    hook_point_name = "blocks.10.hook_resid_pre"
+    batch = get_batch()
+    pca_dirs = get_pca_dirs()
+
+    get_kl = partial(
+        get_kl_diff_permuting_dir,
+        model=gpt2,
+        hook_point_name=hook_point_name,
+        device=device,
+        input_ids=batch,
+    )
+    xs = range(25)
+    kls = [get_kl(dir=pca_dirs[x]) for x in tqdm.tqdm(xs)]
+
+    plt.plot(xs, kls, "o-k")
+    plt.ylim(0, None)
+    plt.xlabel("PCA direction")
+    plt.ylabel("KL divergence")
+    # plt.title("How much does permuting activations mess up the model?")
+    plt.gcf().set_size_inches(4, 3)
+
+    scripts_dir = Path(__file__).parent
+    out_dir = scripts_dir / "out/pca_dir_0"
+
+    plt.savefig(out_dir / "resample_sensitivity.png", dpi=300, bbox_inches="tight")
+    plt.savefig(out_dir / "resample_sensitivity.svg", bbox_inches="tight")
